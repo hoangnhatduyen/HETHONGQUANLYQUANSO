d4d415e6752e54acfccf29c1ec35b611f46d20d6
Hoang Nhat Duyen <hoangnhatduyensnd@outlook.com>
2023-12-18 09:38:38 +0700

    kk

--- HETHONGQUANLYQUANSO/Controllers/QuanNhanController.cs
+++ HETHONGQUANLYQUANSO/Controllers/QuanNhanController.cs
@@ -22,18 +22,87 @@ namespace HETHONGQUANLYQUANSO.Controllers
             return View(quanNhanList);
 
         }
-        public void AddQuanNhan(QuanNhan quanNhan)
+        [HttpPost]
+        public IActionResult AddQuanNhan(QuanNhan quanNhan)
         {
-            // Kiểm tra xem quanNhan đã tồn tại trong cơ sở dữ liệu chưa
-            var existingQuanNhan = HTQLQSContext.QuanNhans.SingleOrDefault(q => q.MaQuanNhan == quanNhan.MaQuanNhan);
-
-            if (existingQuanNhan == null)
+            try
             {
-                HTQLQSContext.QuanNhans.Add(quanNhan);
-                HTQLQSContext.SaveChanges();
-            }
-            // Lưu thay đổi vào cơ sở dữ liệu
+                // Kiểm tra xem quanNhan đã tồn tại trong cơ sở dữ liệu chưa
+                var existingQuanNhan = HTQLQSContext.QuanNhans.SingleOrDefault(q => q.MaQuanNhan == quanNhan.MaQuanNhan);
 
+                if (existingQuanNhan == null)
+                {
+                    HTQLQSContext.QuanNhans.Add(quanNhan);
+                    HTQLQSContext.SaveChanges();
+                    return Json(new { success = true, message = "Thêm quân nhân thành công!" });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Mã quân nhân đã tồn tại trong cơ sở dữ liệu!" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi khi thêm quân nhân: " + ex.Message });
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateQuanNhan(QuanNhan quanNhan)
+        {
+            try
+            {
+                // Kiểm tra xem quanNhan có tồn tại trong cơ sở dữ liệu không
+                var existingQuanNhan = HTQLQSContext.QuanNhans.SingleOrDefault(q => q.MaQuanNhan == quanNhan.MaQuanNhan);
+
+                if (existingQuanNhan != null)
+                {
+                    // Cập nhật thông tin quân nhân
+                    existingQuanNhan.HoVaTen = quanNhan.HoVaTen;
+                    existingQuanNhan.CapBac = quanNhan.CapBac;
+                    existingQuanNhan.MaChucVu = quanNhan.MaChucVu;
+                    existingQuanNhan.MaDonVi = quanNhan.MaDonVi;
+                    existingQuanNhan.GhiChu = quanNhan.GhiChu;
+
+                    HTQLQSContext.SaveChanges();
+                    return Json(new { success = true, message = "Cập nhật thông tin quân nhân thành công!" });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy quân nhân có mã " + quanNhan.MaQuanNhan });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi khi cập nhật quân nhân: " + ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteQuanNhan(string maQuanNhan)
+        {
+            try
+            {
+                // Kiểm tra xem quanNhan có tồn tại trong cơ sở dữ liệu không
+                var existingQuanNhan = HTQLQSContext.QuanNhans.SingleOrDefault(q => q.MaQuanNhan == maQuanNhan);
+
+                if (existingQuanNhan != null)
+                {
+                    // Xóa quân nhân
+                    HTQLQSContext.QuanNhans.Remove(existingQuanNhan);
+                    HTQLQSContext.SaveChanges();
+                    return Json(new { success = true, message = "Xóa quân nhân thành công!" });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy quân nhân có mã " + maQuanNhan });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi khi xóa quân nhân: " + ex.Message });
+            }
         }
 
 
@@ -49,12 +118,11 @@ namespace HETHONGQUANLYQUANSO.Controllers
 
                 case "sua":
                     // Xử lý logic sửa quân nhân
-                    //QuanNhanController.UpdateQuanNhan(quanNhan);
+                    UpdateQuanNhan(quanNhan);
                     break;
 
                 case "xoa":
-                    // Xử lý logic xóa quân nhân
-                    //QuanNhanController.DeleteQuanNhan(quanNhan.MaQuanNhan);
+                    DeleteQuanNhan(quanNhan.MaQuanNhan);
                     break;
 
                     // Các trường hợp khác nếu cần
